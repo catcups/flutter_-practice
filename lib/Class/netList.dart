16f440a0a8848f6e59d5b6bb3ea25eae4799a2fe
catcups <1191948005@qq.com>
2019-08-12 12:02:16 +0800

    fix list

--- lib/Class/netList.dart
+++ lib/Class/netList.dart
@@ -2,19 +2,28 @@ import 'package:flutter/material.dart';
 
 class NetPage extends StatefulWidget {
   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return ShowNetPage();
+  }
+}
+
+
+class ShowNetPage extends State<NetPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: new Scaffold(
+        appBar: AppBar(title: Text('网络列表Demo'),),
         body: Text(
           'kankankankanakankana'
         ),
       ),
     );
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return null;
   }
 }

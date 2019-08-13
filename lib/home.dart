af3f6b891931b3fb3805274267559103432bb85a
catcups <1191948005@qq.com>
2019-08-13 12:07:26 +0800

    up

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as prefix0;
 
 void main()=>runApp(new HomeFrament());
 
@@ -11,6 +14,7 @@ class HomeFrament extends StatefulWidget{
 }
 
 class _HomeFramentState extends State<HomeFrament> {
+  bool isVisible=false;
   @override
   void initState() {
     super.initState();
@@ -22,17 +26,81 @@ class _HomeFramentState extends State<HomeFrament> {
       theme: ThemeData(primaryColor: Color.fromARGB(255, 255, 123, 155)),
       home: Scaffold(
         appBar: AppBar(
+          elevation: 0.0,
         title: Row(children: <Widget>[
           // Image.asset('WX20190619-192036@2x'),
           ClipOval(
-            child: Image.asset('WX20190619-192036@2x.png', width: 35.0, height: 35.0,),
+            child: Image.asset('WX20190619-192036@2x.png', width: MediaQueryData.fromWindow(window).padding.top, height: 35.0,),
           ),
-          ClipOval(
-            child: Image.asset("WX20190619-192036@2x.png", width: 15.0, height: 15.0,),
+          Container(
+            // alignment: Alignment(-.8, 0),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(left: 13.0, right: 13.0),
+            padding: EdgeInsets.only(left: 4.0),
+            child: Icon(Icons.search, color: Colors.white,),
+            width: 180,
+            height: 30,
+            decoration: BoxDecoration(
+              color: Colors.black26,
+              borderRadius: BorderRadius.all(Radius.circular(15.0))
+            ),
+          ),
+          Image.asset('WX20190619-192036@2x.png', width: MediaQueryData.fromWindow(window).padding.top, height: 35.0,),
+          Padding(
+            padding: EdgeInsets.only(left: 15.0),
+            child: Icon(Icons.settings_applications,
+            color: Colors.white,),
+          ),
+          Padding(
+            padding: EdgeInsets.only(left: 25.0),
+            child: Icon(
+              Icons.message,
+              color: Colors.white
+            ),
           )
-          ,Text('data')
         ],),
         centerTitle: true,
+        ),
+        body: new ListView(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(top: 13.0),
+              alignment: Alignment.center,
+              color: Colors.white,
+              height: 50,
+              child:
+              // width: MediaQueryData.fromWindow(window).padding.top,)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Text('直播',),
+                  Container(height: 1.0,width: 30.0,color: Colors.redAccent,margin: EdgeInsets.only(top: 5.0),)
+                ],),
+                Column(children: <Widget>[
+                  Text('直播'),
+                  Container(height: 1.0,width: 30.0,color:isVisible?Colors.redAccent:Colors.white,margin: EdgeInsets.only(top: 5.0),)
+                ],),
+                Column(children: <Widget>[
+                  Text('直播'),
+                  Container(height: 1.0,width: 30.0,color:isVisible?Colors.redAccent:Colors.white,margin: EdgeInsets.only(top: 5.0),)
+                ],),
+                Column(children: <Widget>[
+                  Text('直播'),
+                  Container(height: 1.0,width: 30.0,color:isVisible?Colors.redAccent:Colors.white,margin: EdgeInsets.only(top: 5.0),)
+                ],),
+                Column(children: <Widget>[
+                  Text('直播'),
+                  Container(height: 1.0,width: 30.0,color:isVisible?Colors.redAccent:Colors.white,margin: EdgeInsets.only(top: 5.0),)
+                ],),
+              ],
+            )
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              child: prefix0.Image.asset('images/20190802190018_ZML4A.jpeg', width: MediaQuery.of(context).size.width,),
+            )
+          ],
         ),
       ),
     );

2823a4e1f7ed2523fc1a9d0c518279cc2c19d004
catcups <1191948005@qq.com>
2019-08-16 18:16:22 +0800

    up main list

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -27,13 +27,15 @@ class FirstPage extends StatelessWidget {
   // 创建一个常量List， 不可以改变的List
   List constList = const['10', 7, 23];
    //  在数组中可以存放多个不同类型的对象
-  var list = ['push传值', '点击', '点击', '点击', '点击', 
-              '点击', '点击', '点击', '点击', '点击', 
-              '点击', '点击', '点击', '点击', '点击', 
-              '点击', '点击', '点击', '点击', '点击'];
+  var list = ['push传值', '点击', '点击1', '点击2', 'Login', 
+              '网络列表', '布局', '输入', 'TabBar', '点击9', 
+              '点击10', '点击11', '点击12', '点击13', '点击14', 
+              '点击15', '点击16', '点击17', '点击18', '点击2333'];
+  var widgets = [];
 
   @override
   Widget build(BuildContext context) {
+    widgets = buildMainTileList(context);
       return new Scaffold(
         appBar: AppBar(
           title: Text('Flutter Practice',),
@@ -42,25 +44,10 @@ class FirstPage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                Column(children: <Widget>[
-                  Icon(Icons.done),
-                  new CupertinoButton(
-                  child: Text('点击push'),
-                  onPressed: () {
-                    // Navigator.pushNamed(context, '/one');
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                      return PagesOne(textData: '传值',);
-                    })).then((value){print(value);});
-                  },
-                  color: Colors.red,
-                ),
-                ],),
-                // ListView.builder(
-                //   itemBuilder: (context, index) {
-                //   return Column(children: <Widget>[
+                // Column(children: <Widget>[
                 //   Icon(Icons.done),
                 //   new CupertinoButton(
-                //   child: Text('点击'), 
+                //   child: Text('点击push'),
                 //   onPressed: () {
                 //     // Navigator.pushNamed(context, '/one');
                 //     Navigator.of(context).push(MaterialPageRoute(builder: (context){
@@ -69,13 +56,64 @@ class FirstPage extends StatelessWidget {
                 //   },
                 //   color: Colors.red,
                 // ),
-                // ],);
-                // },itemCount: 2,),
-                Column(children: <Widget>[
-                  new CupertinoButton(
-                  child: Text('点击'), 
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                // ],),
+
+                Container(
+                  width:MediaQuery.of(context).size.width / 2,
+                  child:ListView.builder(
+                    itemBuilder: (context, index) {
+                    return Column(children: <Widget>[
+                    Icon(Icons.done),
+                    new CupertinoButton(
+                    child: Text('点击'), 
+                    onPressed: () {
+                      // Navigator.pushNamed(context, '/one');
+                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
+                        return PagesOne(textData: '传值',);
+                      })).then((value){print(value);});
+                    },
+                    color: Colors.red,
+                  ),
+                  ],);
+                  },itemCount: 2,),
+                ),
+                // Column(
+                //       children: buildMainTileList(context),
+                //     )
+                Container(
+                  width:MediaQuery.of(context).size.width / 2,
+                  child:
+                  ListView.builder(
+                    itemBuilder: (context, index) {
+                    // return buildMainTileList(context)[index]; // 不适用 多次创建widget了
+                    return widgets[index];
+                  },itemCount: list.length-1,),
+                  )
+                ],
+          ),
+    );
+  }
+
+  List<Widget> buildMainTileList(BuildContext context) {
+    List<Widget> widgetList = new List();
+      for (int i = 1; i < list.length; i++) {
+        print('\n创建widget $i');
+        widgetList.add(
+          new CupertinoButton(
+          child: Text(list[i]), 
+          onPressed: () {
+            pushNext(context, i);
+          },
+          color: Colors.red,
+        ));
+      }
+    return widgetList;
+  }
+
+  pushNext(BuildContext context, int index) {
+    switch (index) {
+      case 1:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
                       return new MaterialApp(
                         title: '2333',
                         theme: new ThemeData(primaryColor: Colors.red),
@@ -95,23 +133,15 @@ class FirstPage extends StatelessWidget {
                       backgroundColor: Color(0xFF9E9E9E),
                       textColor: Color(0xFFffffff));
                     });
-                  },
-                  color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('点击'), 
-                    onPressed: () {
-                      // Navigator.pushNamed(context, '/one');
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                        return PagesTwo(textData: '233',);
-                      })).then((value){print(value);});
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('点击2'), 
-                    onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context){
+        break;
+      case 2:
+        // Navigator.pushNamed(context, '/one');
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+          return PagesTwo(textData: '233',);
+        })).then((value){print(value);});
+        break;
+      case 3:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
                         return new Scaffold(
                             appBar: new AppBar(title: new Text('23ssdw'),),
                             floatingActionButton: FloatingActionButton(onPressed: () {
@@ -127,57 +157,69 @@ class FirstPage extends StatelessWidget {
                         backgroundColor: Color(0xFF9E9E9E),
                         textColor: Color(0xFFffffff));
                       });
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('Login'), 
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
+        break;
+      case 4:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
                         return MyAppF();
                       }));
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('网络列表'), 
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
+        break;
+      case 5:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
                         return NetPage();
                       }));
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('布局'), 
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
+        break;
+      case 6:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
                         return HomeFrament();
-                      }));
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('输入'), 
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                        return EditTextWidget();
-                      }));
-                    },
-                    color: Colors.red,
-                  ),
-                  new CupertinoButton(
-                    child: Text('TabBar'), 
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context){
-                        return MainTabBar();
-                      }));
-                    },
-                    color: Colors.red,
-                  ),
-                  ])
-                ],
-          ),
-    );
+            }));
+        break;
+      case 7:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 8:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return MainTabBar();
+            }));
+        break;
+      case 9:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 10:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 11:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 12:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 13:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 14:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      case 15:
+        Navigator.of(context).push(MaterialPageRoute(builder: (context){
+              return EditTextWidget();
+            }));
+        break;
+      default:
+    }
   }
+
 }

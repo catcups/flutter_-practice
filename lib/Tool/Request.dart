972cef714f5c3e2c46292e454a771ad2486348d1
catcups <1191948005@qq.com>
2019-08-16 18:41:04 +0800

    add request

---
+++ lib/Tool/Request.dart
@@ -0,0 +1,54 @@
+
+class AccountEntity {
+	String accessToken;
+	String refreshToken;
+
+	AccountEntity({this.accessToken, this.refreshToken});
+
+	AccountEntity.fromJson(Map<String, dynamic> json) {
+		accessToken = json['access_token'];
+		refreshToken = json['refresh_token'];
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		data['access_token'] = this.accessToken;
+		data['refresh_token'] = this.refreshToken;
+		return data;
+	}
+}
+
+class UserEntity {
+	String avatarUrl;
+	String name;
+	int id;
+	String blog;
+
+	UserEntity({this.avatarUrl, this.name, this.id, this.blog});
+
+	UserEntity.fromJson(Map<String, dynamic> json) {
+		avatarUrl = json['avatar_url'];
+		name = json['name'];
+		id = json['id'];
+		blog = json['blog'];
+	}
+
+	Map<String, dynamic> toJson() {
+		final Map<String, dynamic> data = new Map<String, dynamic>();
+		data['avatar_url'] = this.avatarUrl;
+		data['name'] = this.name;
+		data['id'] = this.id;
+		data['blog'] = this.blog;
+		return data;
+	}
+}
+
+
+class BaseEntity<T>{
+
+  int code;
+  String message;
+  T data;
+  
+  BaseEntity(this.code, this.message, this.data);
+}

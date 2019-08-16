a5d81c4227279d80d0be90a845050aee520a38c2
catcups <1191948005@qq.com>
2019-08-16 16:09:01 +0800

    up

--- lib/Class/TabBar.dart
+++ lib/Class/TabBar.dart
@@ -6,6 +6,7 @@ class MainTabBar extends StatefulWidget {
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MainTabBarState();
+    return _MainTabBarState1();
   }
 }
 
@@ -13,10 +14,71 @@ class _MainTabBarState extends State<MainTabBar> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(body:
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text('看啥子'),
+    //   ),
+    //   body:
+    return 
     DefaultTabController(
       length: 4,
-      child: Column(
+      child: new Scaffold(
+        appBar: AppBar(
+        title: Text('看啥子'),
+      ),
+        body:
+        Column(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(top: 5, bottom: 1),
+            width: 600,
+            height: 60,
+            color: Colors.white,
+            child: TabBar(
+              labelColor: Colors.blue,
+              unselectedLabelColor: Colors.black,
+              indicatorColor: Colors.redAccent,
+              indicatorWeight: 10,
+              isScrollable: false,
+              labelStyle: TextStyle(fontSize: 14),
+              tabs: <Widget>[
+                Tab(text: 'AAA',),
+                Tab(text: 'BBB',),
+                Tab(text: 'CCC',),
+                Tab(text: 'DDD',),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: TabBarView(
+              children: <Widget>[
+                Page111(),
+                NetPage3(),
+                Page111(),
+                NetPage3()
+              ],
+            ),
+          )
+        ],
+      ),)
+    );//);
+  }
+}
+
+class _MainTabBarState1 extends State<MainTabBar> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('看啥子'),
+      ),
+      body:
+    DefaultTabController(
+      length: 4,
+      child:
+        Column(
         children: <Widget>[
           Container(
             margin: EdgeInsets.only(top: 5, bottom: 1),
@@ -65,5 +127,7 @@ class Page111 extends StatelessWidget {
       child: Text('data', style: TextStyle(fontSize: 20, color: Colors.red),),
     );
   }
-  
 }
+
+
+

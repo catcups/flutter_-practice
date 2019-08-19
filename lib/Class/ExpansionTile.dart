dde30a1d951d9c984742d63b4484729e2156ea26
catcups <1191948005@qq.com>
2019-08-19 16:31:48 +0800

    add dio

--- lib/Class/ExpansionTile.dart
+++ lib/Class/ExpansionTile.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 class ExpansionTileList extends StatefulWidget {
   @override
@@ -8,7 +12,59 @@ class ExpansionTileList extends StatefulWidget {
   }
 }
 
+
 class _ExpansionTileListState extends State<ExpansionTileList> {
+
+  var _ipAddress = 'Unknown';
+
+  _getIPAddress() async {
+    var url = 'https://httpbin.org/ip';
+    var httpClient = new HttpClient();
+
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.OK) {
+        var json = await response.transform(utf8.decoder).join();
+        var data = jsonDecode(json);
+        result = data['origin'];
+        print('请求 $data');
+      } else {
+        result =
+            'Error getting IP address:\nHttp status ${response.statusCode}';
+            print('请求 $result');
+      }
+    } catch (exception) {
+      result = 'Failed getting IP address';
+      print('请求 $result');
+    }
+
+    // If the widget was removed from the tree while the message was in flight,
+    // we want to discard the reply rather than calling setState to update our
+    // non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _ipAddress = result;
+    });
+  }
+
+  void getHttp() async {
+  try {
+    Response response = await Dio().get("https://api.acplay.net/api/v2/search/episodes?anime=%E5%A6%96%E7%B2%BE%E7%9A%84%E5%B0%BE%E5%B7%B4");
+    print(response);
+  } catch (e) {
+    print(e);
+  }
+}
+  @override
+  void initState() {
+    _getIPAddress();
+    getHttp();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -17,10 +73,86 @@ class _ExpansionTileListState extends State<ExpansionTileList> {
       theme: new ThemeData(primaryColor: Colors.red),
       home: new Scaffold(
         appBar: new AppBar(title: new Text('List'),),
-        floatingActionButton: FloatingActionButton(onPressed: () {
+        body: ListView.builder(
+          itemBuilder: (BuildContext content, int index) => EntryItem(data[index]),
+          itemCount: data.length,
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Text('Back'),
+          onPressed: () {
             Navigator.pop(context, 'Toast');
         }),
       ),
     );
   }
 }
+
+class EntryItem extends StatelessWidget {
+  const EntryItem(this.entry);
+
+  final Entry entry;
+
+  Widget _buildTiles(Entry root) {
+    if (root.children.isEmpty) return ListTile(title: Text(root.title));
+    return ExpansionTile(
+      key: PageStorageKey<Entry>(root),
+      title: Text(root.title),
+      children: root.children.map(_buildTiles).toList(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return _buildTiles(entry);
+  }
+}
+
+class Entry {
+  const Entry(this.title, [this.children = const <Entry>[]]);
+  final String title;
+  final List<Entry>children;
+}
+
+// Data to display
+const List<Entry> data = <Entry>[
+  Entry(
+    'AAA',
+    <Entry>[
+      Entry('AAA-a',
+      <Entry>[
+        Entry('AAA-a-0'),
+        Entry('AAA-a-1'),
+        Entry('AAA-a-2'),
+      ]
+      ),
+      Entry('AAA-b',
+      <Entry>[
+        Entry('AAA-b-0'),
+        Entry('AAA-b-1'),
+      ]
+      ),
+      Entry('AAA-c'),
+    ]
+  ),
+    Entry(
+    'BBB',
+    <Entry>[
+      Entry('BBB-a',
+      <Entry>[
+        Entry('BBB-a-0'),
+        Entry('BBB-a-2'),
+      ]
+      ),
+      Entry('BBB-b',
+      <Entry>[
+        Entry('BBB-b-0'),
+        Entry('BBB-b-1'),
+      ]
+      ),
+      Entry('BBB-c'),
+    ]
+  ),
+];
+
+
--- lib/Tool/Request.dart
+++ lib/Tool/Request.dart
@@ -1,3 +1,14 @@
+import 'dart:io';
+var httpClient = new HttpClient();
+
+get() async {
+  var httpClient = new HttpClient();
+  var uri = new Uri.http(
+      'example.com', '/path1/path2', {'param1': '42', 'param2': 'foo'});
+  var request = await httpClient.getUrl(uri);
+  var response = await request.close();
+  // var responseBody = await response.transform(UTF8.decoder).join();
+}
 
 class AccountEntity {
 	String accessToken;
@@ -52,3 +63,4 @@ class BaseEntity<T>{
   
   BaseEntity(this.code, this.message, this.data);
 }
+
